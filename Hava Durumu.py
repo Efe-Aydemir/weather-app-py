7f46df31d5c0c67153917724debcce7976c24815
TheMonJoR <81474037+TheMonJoR@users.noreply.github.com>
2021-10-10 13:14:39 +0300

    Add files via upload

---
+++ Hava Durumu.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import time
+
+# r = requests.get("http://api.weatherapi.com/v1/current.json?key=e8d78d472edb4333888101025210910&q=Turkey/Ankara&aqi=no")
+
+# res = r.text
+
+# data = json.loads(res)
+
+
+# derece = data["current"]["temp_c"]
+
+# sehir = data["location"]["name"]
+
+
+
+# print(f"Bugün {sehir}'de hava {derece} derece.")
+
+while True:
+    print("-- Hava Durumu App --")
+
+    print("_"*31)
+
+    sehir = input("Şehir: ")
+
+    sehir.capitalize()
+
+    r2 = requests.get(f"http://api.weatherapi.com/v1/current.json?key=e8d78d472edb4333888101025210910&q=Turkey/{sehir}&aqi=no")
+
+    res2 = r2 .text
+
+    data2 = json.loads(res2)
+
+    derece2 = data2["current"]["temp_c"]
+    sehir2 = data2["location"]["name"]
+
+
+    print(f"Şehir: {sehir2}\nDerece: {derece2} ")
+
+    soru = input("Program Devam Ettirilsinmi (y/n): ")
+
+    if soru == "y":
+        print("_"*31)
+        continue
+    else:
+        print("Çıkış Yapılıyor....")
+        time.sleep(5)
+        break
